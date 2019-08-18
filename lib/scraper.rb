1d6825c1c9592019b201082b0b19ad614d85cff9
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-18 03:04:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-doc.css(".site-header__hero__headline")
+
 
 puts doc.css(".site-header__hero__headline")
